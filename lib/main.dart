a0b8ae13a2d5d6a364be100663553f3fd2eb65b7
Manikandan <chemanishazad@gmail.com>
2024-12-17 16:49:41 +0530

    google auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:dating/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
