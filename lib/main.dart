9086f429e81eea017d56dbbc43cccd19fc28c714
Manikandan <chemanishazad@gmail.com>
2024-12-17 16:38:19 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Sign-In Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    // Listen to GoogleSignIn events to update state
+    GoogleSignIn().onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    GoogleSignIn().signInSilently();
+  }
+
+  Future<void> _handleSignOut() async {
+    try {
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
+  Future<void> _handleGoogleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return; // User cancelled the sign-in.
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      if (userCredential.user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => UserDetailsScreen(user: googleUser),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error signing in with Google: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final GoogleSignInAccount? user = _currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Sign-In Demo'),
+      ),
+      body: Center(
+        child: user != null
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(user.photoUrl ?? ''),
+                    radius: 40,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Welcome, ${user.displayName}!',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text('Email: ${user.email}'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _handleSignOut,
+                    child: const Text('Sign Out'),
+                  ),
+                ],
+              )
+            : ElevatedButton(
+                onPressed: _handleGoogleSignIn,
+                child: const Text('Sign In with Google'),
+              ),
+      ),
+    );
+  }
+}
+
+class UserDetailsScreen extends StatelessWidget {
+  final GoogleSignInAccount user;
+
+  const UserDetailsScreen({super.key, required this.user});
+
+  Future<void> _handleSignOut(BuildContext context) async {
+    try {
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MyHomePage(),
+          ));
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(user);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Details'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.photoUrl ?? ''),
+              radius: 40,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Welcome, ${user.displayName}!',
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text('Email: ${user.email}'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _handleSignOut(context),
+              child: const Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
